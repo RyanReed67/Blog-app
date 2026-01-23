10796b5bc7b71826f60a88c82a25f57fd4f19ad6
Ryan Reed <ryno5454@gmail.com>
2026-01-22 19:07:03 -0600

    Refactor routes to use async/await and database queries

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-// import sql from "./db.js";
 
 const app = express();
 const port = 3000;
@@ -27,50 +26,81 @@ async function loadPosts() {
     return result.rows;
 }
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+    try {
+       posts = await loadPosts(); 
+        
+       const result = await db.query('SELECT * FROM "BlogPosts" ORDER BY id DESC');
+       res.render("index.ejs", { posts: result.rows });
+    } catch (err) {
+        console.error("Error loading posts:", err);
+        res.render("index.ejs", { posts: [] }); 
+    }
+});
+
+app.post("/submit", async (req, res) => {
+    const { title, content } = req.body;
     
-    posts = loadPosts();
-    console.log(posts);
-    res.render("index.ejs", {posts: posts});
-});
-
-app.post("/submit", (req, res) => {
-    const {title, content} = req.body;
-    const date = new Date().toLocaleString();
-    posts.push({
-        title,
-        content,
-        date: date
-    });
-    // sql `INSERT into "BlogPosts" (title, content, date) values (${title}, ${content}, ${date})`
-    res.redirect("/");
-});
-
-app.get("/edit/:index", (req, res) => {
-    const index = req.params.index;
-    const postToEdit = posts[index];
-    if (postToEdit) {
-        res.render("edit.ejs", {
-            post: postToEdit,
-            index: index
-        });
-    } else {
+    try {
+        await db.query(
+            'INSERT INTO "BlogPosts" (title, content) VALUES ($1, $2)', 
+            [title, content]
+        );
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
         res.redirect("/");
     }
 });
 
-app.post("/update", (req, res) => {
-    const {index, title, content} = req.body;
-    posts[index] = {title, content};
-    res.redirect("/");
+app.get("/edit/:id", async (req, res) => {
+    const id = req.params.id;
+
+    try {
+        const result = await db.query('SELECT * FROM "BlogPosts" WHERE id = $1', [id]);
+        const postToEdit = result.rows[0];
+
+        if (postToEdit) {
+            res.render("edit.ejs", {
+                post: postToEdit
+            });
+        } else {
+            res.redirect("/");
+        }
+    } catch (err) {
+        console.error(err);
+        res.redirect("/");
+    }
 });
 
-app.post("/delete", (req, res) => {
-    const index = req.body.index;
-    posts.splice(index, 1);
-    res.redirect("/");
+app.post("/update", async (req, res) => {
+    const { id, title, content } = req.body;
+
+    try {
+        await db.query(
+            'UPDATE "BlogPosts" SET title = $1, content = $2 WHERE id = $3',
+            [title, content, id]
+        );
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.redirect("/");
+    }
+});
+
+app.post("/delete", async (req, res) => {
+    const idToDelete = req.body.id;
+
+    try {
+        await db.query('DELETE FROM "BlogPosts" WHERE id = $1', [idToDelete]);
+        res.redirect("/");
+    } catch (err) {
+        console.error(err);
+        res.redirect("/");
+    }
 });
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
